bcd7d702f6b9e270bf9558478ff897608b05f18e
shojs777 <shokiebi2561@gmail.com>
2022-05-10 02:51:54 +0900

    Headerコンポーネント完成

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import Style from "./Header.module.scss";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <>
+      <div className={Style.header}>
+        <div className={Style.item}>
+          <Link to="/">VideoTube</Link>
+        </div>
+        <div className={Style.item}>
+          <form>
+            <input type="text" placeholder="検索" />
+            <button type="submit">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
